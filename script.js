8ef7d2bedb9f8adae7eaafc8a353851278064eee
Moshelle Garbla <moshellegg@gmail.com>
2021-10-28 06:43:23 -0400

    added operations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const display1E1 = document.querySelector('.display-1');
 const display2E1 = document.querySelector('.display-2');
 const tempResultE1 = document.querySelector('.temp-result');
-const numberE1 = document.querySelectorAll('.number');
+const numbersE1 = document.querySelectorAll('.number');
 const operationE1 = document.querySelectorAll('.operation');
 const equalE1 = document.querySelector('.equal');
 const clearE1 = document.querySelector('.all-clear');
@@ -16,6 +16,33 @@ let haveDot = false;
 
 numbersE1.forEach( number => {
     number.addEventListener('click', (e)=> {
+if(e.target.innerText === '.' && !haveDot){
+  haveDot = true;
+      }else if(e.target.innerText === '.' && haveDot){
+       return;  
+      }
+      dis2Num += e.target.innerText;
+      display2E1.innerText = dis2Num
 
     })
 })
+
+operationE1.forEach( operation =>{
+    operation.addEventListener('click', (e)=>{
+      if (!dis2Num) return;
+      haveDot= false;
+      const operationName = e.target.innerText;
+      if(dis1Num && dis2Num && lastOperation){
+        mathOperation();
+      }else{
+          result = parseFloat(dis2Num)
+      }
+      clearVar(operationName);
+      console.log(result);
+    })
+});
+
+function clearVar(name = ''){
+    dis1Num += dis2Num+ ' ' + name + ' ';
+    display1E1.innerText = dis1Num;
+}
